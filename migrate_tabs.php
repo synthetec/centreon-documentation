e96146099e9243fd2280dc2836443e923679d9a2
Kevin Duret <kduret@centreon.com>
2021-11-26 10:21:26 +0100

    add script skeleton

---
+++ migrate_tabs.php
@@ -0,0 +1,16 @@
+<?php
+
+// scan files which end with .md (scandir)
+
+foreach ($files as $file) {
+    $content = file_get_contents($file);
+    if (preg_match($content, regex, $matches)) {
+        var_dump($matches);
+        // replace content by new tabs format
+        $content = str_replace($oldTabs, $newTabs, $content);
+    }
+    file_put_contents($file, $content);
+}
+
+
+
